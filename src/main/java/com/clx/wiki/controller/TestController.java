e5985ce3490f344974a6d06538f9f96ee058ca7f
clx1899 <1677430883@qq.com>
2021-08-29 15:27:42 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/clx/wiki/controller/TestController.java
+++ src/main/java/com/clx/wiki/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.clx.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 /**
  * @author clx
@@ -19,6 +21,12 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world, post!" + name;
     }
 
 
