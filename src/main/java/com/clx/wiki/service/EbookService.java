3e0ffef0ec556550b96cbe9ce0ef7e2dbd632e34
clx1899 <1677430883@qq.com>
2021-08-30 23:12:35 +0800

    6-4 集成PageHelper插件 将mapper日志级别改为trace 打印所有的sql日志

--- src/main/java/com/clx/wiki/service/EbookService.java
+++ src/main/java/com/clx/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.clx.wiki.mapper.EbookMapper;
 import com.clx.wiki.req.EbookReq;
 import com.clx.wiki.resp.EbookResp;
 import com.clx.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -19,18 +23,26 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
+
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req){
+        PageHelper.startPage(1,3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
 
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 //            //EbookResp ebookResp = new EbookResp();
