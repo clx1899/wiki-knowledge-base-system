d09114be495d8e16fc6aac0b4cfa907d7a9a5bab
clx1899 <1677430883@qq.com>
2021-08-31 14:23:18 +0800

    7-4 分类表格按树形结构展示 增加数组转树形数据工具方法

--- web/src/util/tool.ts
+++ web/src/util/tool.ts
@@ -2,7 +2,7 @@ export class Tool {
   /**
    * 空校验 null或""都返回true
    */
-  public static isEmpty (obj: any) {
+  public static isEmpty(obj: any) {
     if ((typeof obj === 'string')) {
       return !obj || obj.replace(/\s+/g, "") === ""
     } else {
@@ -13,7 +13,7 @@ export class Tool {
   /**
    * 非空校验
    */
-  public static isNotEmpty (obj: any) {
+  public static isNotEmpty(obj: any) {
     return !this.isEmpty(obj);
   }
 
@@ -21,7 +21,7 @@ export class Tool {
    * 对象复制
    * @param obj
    */
-  public static copy (obj: object) {
+  public static copy(obj: object) {
     if (Tool.isNotEmpty(obj)) {
       return JSON.parse(JSON.stringify(obj));
     }
@@ -53,21 +53,4 @@ export class Tool {
     return result;
   }
 
-  /**
-   * 随机生成[len]长度的[radix]进制数
-   * @param len
-   * @param radix 默认62
-   * @returns {string}
-   */
-  public static uuid (len: number, radix = 62) {
-    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-    const uuid = [];
-    radix = radix || chars.length;
-
-    for (let i = 0; i < len; i++) {
-      uuid[i] = chars[0 | Math.random() * radix];
-    }
-
-    return uuid.join('');
-  }
 }
