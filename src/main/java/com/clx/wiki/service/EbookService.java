1a60dd150debd703db4409dfeef5dda1317f36ed
clx1899 <1677430883@qq.com>
2021-08-31 11:03:57 +0800

    6-10 增加删除电子书功能

--- src/main/java/com/clx/wiki/service/EbookService.java
+++ src/main/java/com/clx/wiki/service/EbookService.java
@@ -85,6 +85,12 @@ public class EbookService {
             //更新
             ebookMapper.updateByPrimaryKey(ebook);
         }
+
     }
+
+    public void delete(Long id) {
+        ebookMapper.deleteByPrimaryKey(id);
+    }
+
 
 }
