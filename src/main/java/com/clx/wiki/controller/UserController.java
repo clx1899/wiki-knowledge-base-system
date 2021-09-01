5dc26af49812fc29b5e9b20c193a02b611561f62
clx1899 <1677430883@qq.com>
2021-09-01 17:18:11 +0800

    9-3 完成用户表基本增删改查功能

---
+++ src/main/java/com/clx/wiki/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.clx.wiki.controller;
+
+import com.clx.wiki.req.UserQueryReq;
+import com.clx.wiki.req.UserSaveReq;
+import com.clx.wiki.resp.CommonResp;
+import com.clx.wiki.resp.UserQueryResp;
+import com.clx.wiki.resp.PageResp;
+import com.clx.wiki.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid UserQueryReq req) {
+        CommonResp<PageResp<UserQueryResp>> resp = new CommonResp<>();
+        PageResp<UserQueryResp> list = userService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        userService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        userService.delete(id);
+        return resp;
+    }
+}
