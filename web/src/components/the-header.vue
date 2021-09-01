5ce7919e090ba1b09e73d312a1bbc9cd2ffc96e8
clx1899 <1677430883@qq.com>
2021-09-02 00:28:17 +0800

    9-10 增加退出登录功能，退出登录时，前端清除store登录信息，后端清除redis登录信息

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -2,12 +2,26 @@
   <a-layout-header class="header">
     <div class="logo" />
 
+
+    <a-popconfirm
+        title="确认退出登录?"
+        ok-text="是"
+        cancel-text="否"
+        @confirm="logout()"
+    >
+      <a class="login-menu" v-show="user.id">
+        <span>退出登录</span>
+      </a>
+    </a-popconfirm>
+
     <a class="login-menu" v-show="user.id">
       <span>您好：{{user.name}}</span>
     </a>
     <a class="login-menu" v-show="!user.id" @click="showLoginModal">
       <span>登录</span>
     </a>
+
+
 
     <a-menu
         theme="dark"
@@ -30,6 +44,8 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+
+
 
     </a-menu>
 
@@ -96,13 +112,28 @@ export default defineComponent({
       });
     };
 
+    // 退出登录
+    const logout = () => {
+      console.log("退出登录开始");
+      axios.get('/user/logout/' + user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登录成功！");
+          store.commit("setUser", {});
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     return {
       loginModalVisible,
       loginModalLoading,
       showLoginModal,
       loginUser,
       login,
-      user
+      user,
+      logout
     }
   }
 });
@@ -112,5 +143,6 @@ export default defineComponent({
 .login-menu {
   float: right;
   color: white;
+  padding-left: 10px;
 }
 </style>
