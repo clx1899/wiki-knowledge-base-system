459420ee7c6f0175d7ae6311b8b30b2ab037eac8
clx1899 <1677430883@qq.com>
2021-09-01 17:32:17 +0800

    9-4 增加校验用户名不能重复，增加自定义异常

--- src/main/java/com/clx/wiki/service/UserService.java
+++ src/main/java/com/clx/wiki/service/UserService.java
@@ -1,19 +1,22 @@
 package com.clx.wiki.service;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.clx.wiki.domain.User;
 import com.clx.wiki.domain.UserExample;
+import com.clx.wiki.exception.BusinessException;
+import com.clx.wiki.exception.BusinessExceptionCode;
 import com.clx.wiki.mapper.UserMapper;
 import com.clx.wiki.req.UserQueryReq;
 import com.clx.wiki.req.UserSaveReq;
-import com.clx.wiki.resp.UserQueryResp;
 import com.clx.wiki.resp.PageResp;
+import com.clx.wiki.resp.UserQueryResp;
 import com.clx.wiki.util.CopyUtil;
 import com.clx.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -69,9 +72,15 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(req.getId())) {
-            // 新增
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            User userDB = selectByLoginName(req.getLoginName());
+            if (ObjectUtils.isEmpty(userDB)) {
+                // 新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            } else {
+                // 用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
         } else {
             // 更新
             userMapper.updateByPrimaryKey(user);
@@ -80,5 +89,17 @@ public class UserService {
 
     public void delete(Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String LoginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(LoginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 }
