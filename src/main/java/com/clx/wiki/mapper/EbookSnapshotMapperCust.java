5a9ff7f11b734d3c4986d844ccbe562d2df0176b
clx1899 <1677430883@qq.com>
2021-09-02 23:04:50 +0800

    11-5 增加定时任务，定时收集数据 要在xml里一次执行多个SQL的话，需要为数据库连接增加参数：allowMultiQueries=true

---
+++ src/main/java/com/clx/wiki/mapper/EbookSnapshotMapperCust.java
@@ -0,0 +1,6 @@
+package com.clx.wiki.mapper;
+
+public interface EbookSnapshotMapperCust {
+
+    public void genSnapshot();
+}
---
+++ src/main/java/com/clx/wiki/service/EbookSnapshotService.java
@@ -0,0 +1,18 @@
+package com.clx.wiki.service;
+
+import com.clx.wiki.mapper.EbookSnapshotMapperCust;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class EbookSnapshotService {
+
+    @Resource
+    private EbookSnapshotMapperCust ebookSnapshotMapperCust;
+
+    public void genSnapshot() {
+        ebookSnapshotMapperCust.genSnapshot();
+    }
+
+}
