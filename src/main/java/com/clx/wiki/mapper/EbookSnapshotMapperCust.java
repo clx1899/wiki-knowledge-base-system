5a9f2d6ef04e83300dfee2d1284be273741cc3df
clx1899 <1677430883@qq.com>
2021-09-02 23:13:08 +0800

    11-6 后端增加获取统计数值接口

--- src/main/java/com/clx/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/clx/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,6 +1,12 @@
 package com.clx.wiki.mapper;
 
+import com.clx.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
